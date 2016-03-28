549755e5d3c6c30293b86def2d5430725419a511
Yuki Sonoda <vega.sonoda@gmail.com>
2016-03-28 09:14:16 +0000

    update view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
 
-  get 'static_pages/hoe'
   get 'static_pages/help'
 
+  get 'static_pages/hoe'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
