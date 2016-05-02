537364847742bb4cff5c672acd36d7080d4848f4
Yuki Sonoda <vega.sonoda@gmail.com>
2016-05-02 05:26:18 +0000

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
