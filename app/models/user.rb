8e824d8ee724e5ba6ed2549516c9ca7b8b5c4e92
yuki2224 <xxx.2224.xxx@gmail.com>
2016-06-15 01:00:33 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -66,6 +67,12 @@ class User < ActiveRecord::Base
   # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  # 試作feedの定義
+  # 完全な実装は第12章「ユーザーをフォローする」を参照してください。
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
